d8e7b59f9eaf9917d316745edc525bdb6da4282a
Diana Angelova <diana.anguelova@gmail.com>
2023-03-14 23:19:33 +0200

    Update 08.perfect-number.js

--- 04Exercise_FunctionsAndStatements/08.perfect-number.js
+++ 04Exercise_FunctionsAndStatements/08.perfect-number.js
@@ -1,6 +1,6 @@
 function isPerfect(number) {
     let sum = 0;
-    for (let i = 1; i < number; i++) {
+    for (let i = 1; i <= number/2; i++) {
         if (number % i == 0) {
             sum += i;
         }
