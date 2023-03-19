e28ef71f77b398f5282d7678142aa78504969a87
Diana Angelova <diana.anguelova@gmail.com>
2023-03-19 17:12:01 +0200

    Create 08 Address Book

---
+++ 05Lab_ObjectsAndClasses/08.address-book.js
@@ -0,0 +1,36 @@
+function addressBookParser(input) {
+    let addressBook = {};
+    for (const line of input) {
+        let tokens = line.split(':');
+        let name = tokens[0];
+        let addressNumber = tokens[1];
+        addressBook[name] = addressNumber;
+
+        //Ако трябваше да оставя оригинала,а не да замествам
+        // if (!addressBook.hasOwnProperty(name)){
+        //     addressBook.name=addressNumber;
+        // }
+    }
+
+    let sortedNames = Object.keys(addressBook)
+        .sort((entryA, entryB) => entryA.localeCompare(entryB
+        ));
+
+    for (const name of sortedNames) {
+        console.log(`${name} -> ${addressBook[name]}`);
+    }
+}
+
+addressBookParser(['Bob:Huxley Rd',
+    'John:Milwaukee Crossing',
+    'Peter:Fordem Ave',
+    'Bob:Redwing Ave',
+    'George:Mesta Crossing',
+    'Ted:Gateway Way',
+    'Bill:Gateway Way',
+    'John:Grover Rd',
+    'Peter:Huxley Rd',
+    'Jeff:Gateway Way',
+    'Jeff:Huxley Rd']
+
+);
