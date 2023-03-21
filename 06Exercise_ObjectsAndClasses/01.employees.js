09e1d7dde9b25a1f2fd3b5faa2f556a0af5fd072
Diana Angelova <diana.anguelova@gmail.com>
2023-03-21 20:28:51 +0200

    Update 01. Employees with reduce function

--- 06Exercise_ObjectsAndClasses/01.employees.js
+++ 06Exercise_ObjectsAndClasses/01.employees.js
@@ -1,17 +1,19 @@
 function employeesParser(input) {
-    let employees = {};
-    for (const line of input) {
-        let name = line;
-        let personalNumber = name.length;
-        employees[name]=personalNumber;
-
-    }
-
+    // let employees = {};
+    // for (const line of input) {
+    //     let name = line;
+    //     let personalNumber = name.length;
+    //     employees[name]=personalNumber;
+    // }
+    //Вместо с for може да редуцирам масива към обект чрез reduce функцията
+    let employees = input.reduce((data, employee) => {
+        data[employee] = employee.length;
+        return data;
+    }, {});
     for (const key in employees) {
         console.log(`Name: ${key} -- Personal Number: ${employees[key]}`);
     }
 }
-
 employeesParser([
         'Silas Butler',
         'Adnaan Buckley',
