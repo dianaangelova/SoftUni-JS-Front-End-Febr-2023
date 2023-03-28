b0719d4d86e3409a1bc561b468ac1e40516afb57
Diana Angelova <diana.anguelova@gmail.com>
2023-03-28 12:15:28 +0300

    Create 07.odd-occurrences.js

---
+++ 06Exercise_ObjectsAndClasses/07.odd-occurrences.js
@@ -0,0 +1,20 @@
+function oddOccurrences(input) {
+    let wordsInInput = input.toLowerCase().split(" ");
+    let words = {};
+    let array =[];
+    for (const word of wordsInInput) {
+        let count = wordsInInput.filter((w) => w === word).length;
+        //асоциативен масив
+        if (count%2!=0 && !words.hasOwnProperty(word)){
+            words[" "+word] = count;
+        }
+    }
+
+    for (const word in words) {
+        process.stdout.write(word);
+    }
+
+
+}
+
+oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#');
