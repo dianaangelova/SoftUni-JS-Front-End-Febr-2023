27f3fd9cb7f9251ec18a72dc862241c53132f5c3
Diana Angelova <diana.anguelova@gmail.com>
2023-04-12 20:30:58 +0300

    Create 02. Sections

--- 08Exercise_DOMAndEvents/02.sections/script.js
+++ 08Exercise_DOMAndEvents/02.sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+    const elements = Array.from(document.getElementsByTagName("body"));
+    const content = document.getElementById("content");
+
+
+    words.forEach((elem) => {
+        console.log(elem);
+        let newDiv = document.createElement('div');
+        let newP = document.createElement('p');
+
+        newP.textContent=elem;
+        newP.style.display="none";
+
+        newDiv.appendChild(newP);
+        content.appendChild(newDiv);
+        newDiv.addEventListener("click", () => {
+            newP.style.display="block";
+        })
+    });
 }
