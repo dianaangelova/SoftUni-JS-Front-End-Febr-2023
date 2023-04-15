b7fc38611b083c229c732cb4a073001dd05905fe
Diana Angelova <diana.anguelova@gmail.com>
2023-04-16 00:02:24 +0300

    Create 04 GitHub Repos

---
+++ 09Lab_HTTPAndREST/04.github-repos/scripts/app.js
@@ -0,0 +1,16 @@
+function loadRepos() {
+    const BASE_URL = "https://api.github.com/users/testnakov/repos";
+    const resultContainer = document.getElementById("res");
+    fetch(BASE_URL, {method: 'GET'})
+        //response.text() returns new promise<string>
+        .then((response) => response.text())
+        .then((data) => {
+
+            resultContainer.textContent = data;
+
+        })
+        //when working with promises we always need catch block
+        .catch((error) => {
+            console.log(error);
+        })
+}
